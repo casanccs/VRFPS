b6e1abd4efbdcc5ee3d2635acb5c2ceda46d35a2
Cristian Sanchez <59101636+casanccs@users.noreply.github.com>
2024-05-13 13:41:00 -0500

    Delete android/build directory

--- android/build/assets/Scripts/messenger.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-signal WALL_HEIGHT(height)
-signal SAVE_WALLS
-signal LOAD_WALLS
--- android/build/build/intermediates/assets/debug/Scripts/target.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-
-func _on_area_3d_body_entered(body):
-	body.queue_free()
-	$Area3D.transform.origin = Vector3(randf_range(-1,0), randf_range(-0.5, 0.5), 0)
